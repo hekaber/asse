079d4a646d12d0a40cca7bc685f745914b5cb146
karl <karl@realforce.ch>
2021-02-24 18:40:52 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    AlbumsListComponent
   ],
   imports: [
     BrowserModule,
