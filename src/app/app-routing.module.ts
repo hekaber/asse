932701d5638c8eb32f64ee1fd446e98d3335f232
karl <karl@realforce.ch>
2021-02-25 13:18:08 +0100

    implement signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'albums', component: AlbumsListComponent },
+  // { path: 'albums/new', component: BookFormComponent },
+  // { path: 'albums/view/:id', component: Album }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,38 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signupUser(email: string, password: string) {
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error)
+          }
+        );
+      }
+    );
+  }
+
+  signOutUser() {
+    firebase.auth().signOut();
+  }
 }
